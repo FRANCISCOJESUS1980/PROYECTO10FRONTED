29cd15076c2be0d0a028d355f7b3aaa84b21e9b7
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-12-16 18:14:52 +0100

    feat cambiando url api

--- src/componentes/Eventos/LogicaEventos/components/services/api.js
+++ src/componentes/Eventos/LogicaEventos/components/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api'
+const API_URL = 'https://vercel.com/francisco-jesus-projects/proyecto-10/api'
 
 const isTokenExpired = (token) => {
   const payload = JSON.parse(atob(token.split('.')[1]))
